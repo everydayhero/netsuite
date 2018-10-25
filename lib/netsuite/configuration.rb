756662a721d40d41b5159334aba4f6e662a5b19d
Brent Woodhead <brent.woodhead@everydayhero.com>
2018-10-25 17:13:21 +1000

    Update wsdl to default to passed parameters.

--- lib/netsuite/configuration.rb
+++ lib/netsuite/configuration.rb
@@ -122,13 +122,14 @@ module NetSuite
       if wsdl
         self.wsdl = wsdl
       else
-        attributes[:wsdl] ||= begin
-          if sandbox
-            "https://webservices.sandbox.netsuite.com/wsdl/v#{api_version}_0/netsuite.wsdl"
-          else
-            "https://#{wsdl_domain}/wsdl/v#{api_version}_0/netsuite.wsdl"
-          end
-        end
+        attributes[:wsdl] ||= "https://#{wsdl_domain}/wsdl/v#{api_version}_0/netsuite.wsdl"
+        # attributes[:wsdl] ||= begin
+        #   if sandbox
+        #     "https://webservices.sandbox.netsuite.com/wsdl/v#{api_version}_0/netsuite.wsdl"
+        #   else
+        #     "https://#{wsdl_domain}/wsdl/v#{api_version}_0/netsuite.wsdl"
+        #   end
+        # end
       end
     end
 
